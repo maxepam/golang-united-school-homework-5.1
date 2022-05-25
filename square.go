1bf47e18f50c26b11d5a2fe64f1005d9496636c2
Max Gor <m.hornostaiev@myfeelix.de>
2022-05-26 01:46:35 +0300

    added solution

--- square.go
+++ square.go
@@ -1,5 +1,7 @@
 package square
 
+import "fmt"
+
 type Point struct {
 	x, y int
 }
@@ -9,14 +11,27 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
-	// implement me
+func (s Square) End() Point {
+	newX := s.start.x + int(s.a)
+	newY := s.start.y - int(s.a)
+	p := Point{x: newX, y: newY}
+
+	return p
 }
 
-func (receiver) Area() uint {
-	// implement me
+func (s Square) Area() uint {
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
-	// implement me
+func (s Square) Perimeter() uint {
+	return s.a * 4
 }
+
+// func main() {
+// 	p := Point{x: 2, y: 3}
+// 	s := Square{start: p, a: 12}
+
+// 	fmt.Println(s.End())
+// 	fmt.Println(s.Area())
+// 	fmt.Println(s.Perimeter())
+// }
